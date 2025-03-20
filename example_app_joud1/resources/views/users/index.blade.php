04c48d1d9e02464fdd15fb9d6c5215bed64d8f57
Mona Salih <monaalobisat@gmail.com>
2025-03-20 15:07:24 +0300

    finish update on work

--- example_app_joud1/resources/views/users/index.blade.php
+++ example_app_joud1/resources/views/users/index.blade.php
@@ -8,8 +8,8 @@
     <title>Document</title>
 </head>
 <body>
-<form action="{{ route('user') }}" method="POST">
-    @csrf  <!-- ضروري لحماية الطلبات -->
+<form action="{{ route('user') }}" method="post" enctype="multipart/form-data">
+    @csrf
     <input type="text" name="name" placeholder="name">
     <input type="email" name="email" placeholder="email">
     <input type="password" name="password" placeholder="password">
@@ -17,8 +17,8 @@
     <input type="text" name="gender" placeholder="gender">
     <input type="file" name="photo" placeholder="photo">
 
-
-    <button type="submit">send </button>
+    <button type="submit">Send</button>
 </form>
+
 </body>
 </html>
